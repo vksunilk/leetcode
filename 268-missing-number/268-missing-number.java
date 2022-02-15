fc1c07da26b8682a52d2d846252e9fa2038e0940
vksunilk <48377240+vksunilk@users.noreply.github.com>
2022-02-15 09:00:06 +0530

    Time: 0 ms (100.00%), Space: 50.9 MB (25.87%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -3,7 +3,7 @@ class Solution {
         int n = nums.length;
         int sum = n*(n+1)/2;
         int currSum = 0;
-        for(int num: nums) currSum+=num;
-        return sum-currSum;
+        for(int num: nums) sum-=num;
+        return sum;
     }
 }
